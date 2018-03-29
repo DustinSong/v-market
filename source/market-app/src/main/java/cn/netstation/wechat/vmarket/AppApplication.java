7f0b837f1d3fe7f99dfcd16a0cc1786c8a18ddee
Dustin Song <dustinsong@outlooke.com>
2018-03-30 02:23:38 +0800

    install v-market

---
+++ source/market-app/src/main/java/cn/netstation/wechat/vmarket/AppApplication.java
@@ -0,0 +1,15 @@
+package cn.netstation.wechat.vmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dustin Song
+ * @date 2018-03-28
+ */
+@SpringBootApplication
+public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
